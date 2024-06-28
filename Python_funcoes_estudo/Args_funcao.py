e93d8a57cf7a3c8f5f2a2146cf28ff465c552d56
Guilherme <guilhermeavellar25@gmail.com>
2024-06-28 14:06:12 -0300

    Ajustes no exemplo para mais intuitivo

--- Python_funcoes_estudo/Args_funcao.py
+++ Python_funcoes_estudo/Args_funcao.py
@@ -6,13 +6,15 @@
 # outro nome como *abacate
 
 # exemplo
-def mediaNs(*args):
+def media_ns(*args):
     soma = 0
     for n in args:
         soma += n
-    return soma / len(args) # len retorna o total de elementos de uma coisa
+    return soma / len(args)
+    # len retorna o total de elementos de uma coisa
+
 
 # pode passar qualquer quantidade de números
-print(f"Média de quanto números quisermos: {mediaNs(1,2,3,9,10)}")
-print(f"Média de quanto números quisermos: {mediaNs(1,2,3,9,10,4,5,6,7,8,9,10,4)}")
-
+print(f"Média de quanto números quisermos (1, 2, 3, 9, 10):\n {media_ns(1, 2, 3, 9, 10)}\n")
+print("Média de quanto números quisermos (1, 2, 3, 9, 10, 4, 5, 6, 7, 8, 9, 10, 4):\n "
+      f"{media_ns(1, 2, 3, 9, 10, 4, 5, 6, 7, 8, 9, 10, 4)}\n")
