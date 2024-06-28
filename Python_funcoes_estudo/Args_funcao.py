cc7284105e47a5a0da3fec932edf2f34be401a8b
Guilherme <guilhermeavellar25@gmail.com>
2024-06-28 13:28:36 -0300

    adição de exemplo da função com args

--- Python_funcoes_estudo/Args_funcao.py
+++ Python_funcoes_estudo/Args_funcao.py
@@ -12,5 +12,7 @@ def mediaNs(*args):
         soma += n
     return soma / len(args) # len retorna o total de elementos de uma coisa
 
+# pode passar qualquer quantidade de números
 print(f"Média de quanto números quisermos: {mediaNs(1,2,3,9,10)}")
+print(f"Média de quanto números quisermos: {mediaNs(1,2,3,9,10,4,5,6,7,8,9,10,4)}")
 
