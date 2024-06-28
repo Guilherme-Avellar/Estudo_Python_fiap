4390d71bd1436c8b600bae61c64824520b14f118
Guilherme <guilhermeavellar25@gmail.com>
2024-06-28 16:11:10 -0300

    exemplo de algoritimo recursivo pela fiap

---
+++ Python_funcoes_estudo/Recurcao.py
@@ -0,0 +1,14 @@
+
+# Algoritimo simple de recurção ensinado pela fiap
+
+# DEFINIÇÃO DO subalgoritmo RECURSIVO
+def contagemRecursiva(inicio: float, limite: float) -> None:
+    if inicio > limite:
+        print("Fim!")
+    else:
+        print(inicio)
+        contagemRecursiva(inicio + 1, limite)
+
+# PROGRAMA PRINCIPAL
+print("Executando a função recursiva...")
+contagemRecursiva(1, 5)
